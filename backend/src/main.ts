e54504923ad855eb78d7e8747aae7af424e3cb3a
Trương Tấn Đạt <ls17189a3.11@gmail.com>
2026-07-03 07:43:20 +0700

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: ['*', 'https://goods-shop-v1.vercel.app'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
